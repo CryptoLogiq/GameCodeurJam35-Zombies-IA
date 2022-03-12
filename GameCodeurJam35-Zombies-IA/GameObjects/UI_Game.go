00f29030fcf5cbffb6650ffc34d29cf694e3049e
Crypto Logiq <orbitaldigital@gmail.com>
2022-03-12 12:04:28 +0100

    little_gui

---
+++ GameCodeurJam35-Zombies-IA/GameObjects/UI_Game.go
@@ -0,0 +1,15 @@
+components {
+  id: "UI_Game"
+  component: "/UserInterface/UI_Game.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
