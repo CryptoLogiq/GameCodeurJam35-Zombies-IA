d9c3740503d5f58705cffb1485f497e9d43c702b
Crypto Logiq <orbitaldigital@gmail.com>
2022-03-19 22:29:26 +0100

    Ombres

--- GameCodeurJam35-Zombies-IA/GameObjects/Humans/Villageois.go
+++ GameCodeurJam35-Zombies-IA/GameObjects/Humans/Villageois.go
@@ -28,6 +28,21 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "Controller_IA"
+  component: "/Scripts/IA_Scripts/Controller_IA.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "sprite1"
   type: "sprite"
@@ -175,6 +190,54 @@ embedded_components {
   "default_animation: \"ilde_down\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "collider"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"default\"\n"
+  "mask: \"default\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: -10.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 19.0\n"
+  "  data: 20.0\n"
+  "  data: 10.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  "bullet: false\n"
   ""
   position {
     x: 0.0
--- GameCodeurJam35-Zombies-IA/Scripts_Lua/EntityManager.lua
+++ GameCodeurJam35-Zombies-IA/Scripts_Lua/EntityManager.lua
@@ -9,9 +9,7 @@ function entitypurgeList()
 	Lst_Entity = {}
 end
 
-
-function newEntity(self, pID, pType) -- self == script self (not GameObject self)
-
+function getEntity(self, pID)
 	-- Search if entity allready exist :
 	for _, entity in ipairs(Lst_Entity) do
 		if entity.ID == pID then
@@ -19,6 +17,13 @@ function newEntity(self, pID, pType) -- self == script self (not GameObject self
 			return entity
 		end
 	end
+	return false
+end
+
+function newEntity(self, pID, pType) -- self == script self (not GameObject self)
+	-- allready exist ?
+	local entity = getEntity(self, pID)
+	if entity then return entity end
 	
 	-- New entity :
 	local entity = {ID=pID, type=pType, index=#Lst_Entity+1} -- identity
@@ -28,6 +33,15 @@ function newEntity(self, pID, pType) -- self == script self (not GameObject self
 	entity.up = false
 	entity.down = false
 	entity.idle = false
+	-- vars
+	if pType == "player" then
+		entity.sprBaseName = "#sprite"
+	elseif pType == "zombies" then
+		entity.sprBaseName = "#sprite_"
+	elseif pType == "villageois" then
+		entity.sprBaseName = "#sprite"
+	end
+	entity.sprBaseType = ""
 	--
 	table.insert(Lst_Entity, entity)
 	--
