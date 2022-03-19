4dbb594e072f8a5f88237c28c6ecf70f53eabc8c
Crypto Logiq <orbitaldigital@gmail.com>
2022-03-19 09:49:54 +0100

    preparing_ia_zombs

---
+++ GameCodeurJam35-Zombies-IA/GameObjects/Zombies/Zombie_2.go
@@ -0,0 +1,30 @@
+components {
+  id: "Attack"
+  component: "/IA_Scripts/Attack.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "Follow"
+  component: "/IA_Scripts/Follow.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
