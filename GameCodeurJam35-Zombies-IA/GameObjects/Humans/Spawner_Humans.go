79f06acdd09cd9ce82b3d4cd37cc2576b91fde58
Crypto Logiq <orbitaldigital@gmail.com>
2022-03-25 18:20:39 +0100

    spawner's

---
+++ GameCodeurJam35-Zombies-IA/GameObjects/Humans/Spawner_Humans.go
@@ -0,0 +1,33 @@
+components {
+  id: "Spawner_Humans"
+  component: "/GameObjects/Humans/Spawner_Humans.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "factory"
+  type: "factory"
+  data: "prototype: \"/GameObjects/Humans/Villageois.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
---
+++ GameCodeurJam35-Zombies-IA/GameObjects/Zombies/Spawner_Zombies.go
@@ -0,0 +1,33 @@
+components {
+  id: "Spawner_Zombies"
+  component: "/GameObjects/Zombies/Spawner_Zombies.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "factory"
+  type: "factory"
+  data: "prototype: \"/GameObjects/Zombies/Zombies.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
